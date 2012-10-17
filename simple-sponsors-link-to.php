7cfdc93e7cd3a51e346f7e16c93af240fda8d63a
Jason Conroy <jason@findingsimple.com>
2012-10-17 16:24:20 +1100

    Update to use object context

--- simple-sponsors-link-to.php
+++ simple-sponsors-link-to.php
@@ -1,6 +1,10 @@
 <?php
 
-/* Based on pages links to plugin by Mark Jaquith http://txfx.net/wordpress-plugins/page-links-to/ */
+/** 
+ * Based on the  'Pages links to' plugin 
+ * by Mark Jaquith 
+ * http://txfx.net/wordpress-plugins/page-links-to/ 
+ */
 
 if ( ! class_exists( 'Simple_Sponsors_Link_To' ) ) {
 
@@ -14,7 +18,9 @@ if ( ! class_exists( 'Simple_Sponsors_Link_To' ) ) {
  * @since 1.0
  */
 function simple_initialize_sponsors_link_to() {
-	Simple_Sponsors_Link_To::init();
+
+	$sponsor_links = new Simple_Sponsors_Link_To();
+	
 }
 add_action( 'init', 'simple_initialize_sponsors_link_to', -1 );
 
@@ -25,33 +31,33 @@ class Simple_Sponsors_Link_To {
 
 	static $post_type_name;
 
-	static $links;
+	var $links;
 
-	static $targets;
+	var $targets;
 	
-	static $targets_on_this_page;
+	var $targets_on_this_page;
 
-	public static function init() { 
+	function __construct() { 
 	
-		self::$text_domain = apply_filters( 'simple_sponsors_text_domain', 'Simple_Sponsors' ); 
+		$this->text_domain = apply_filters( 'simple_sponsors_text_domain', 'Simple_Sponsors' ); 
 	
-		self::$post_type_name = apply_filters( 'simple_sponsors_post_type_name', 'simple_sponsor' );		
+		$this->post_type_name = apply_filters( 'simple_sponsors_post_type_name', 'simple_sponsor' );		
 
-		add_action( 'add_meta_boxes', array( __CLASS__, 'add_meta_box' ) );
+		add_action( 'add_meta_boxes', array( $this, 'add_meta_box' ) );
 		
-		add_action( 'save_post', array( __CLASS__, 'save_meta' ), 10, 1 );
+		add_action( 'save_post', array( $this, 'save_meta' ), 10, 1 );
 		
-		add_filter( 'post_type_link', array( __CLASS__, 'filter_link' ), 20, 2 );
+		add_filter( 'post_type_link', array( $this, 'filter_link' ), 20, 2 );
 
-		add_filter( 'wp_list_pages', array( __CLASS__, 'wp_list_pages' ) );
+		add_filter( 'wp_list_pages', array( $this, 'wp_list_pages' ) );
 		
-		add_action( 'template_redirect', array( __CLASS__, 'template_redirect' ) );
+		add_action( 'template_redirect', array( $this, 'template_redirect' ) );
 		
-		add_filter( 'wp_nav_menu_objects', array( __CLASS__, 'wp_nav_menu_objects' ), 10, 2 );
+		add_filter( 'wp_nav_menu_objects', array( $this, 'wp_nav_menu_objects' ), 10, 2 );
 		
-		add_action( 'load-post.php', array( __CLASS__, 'load_post' ) );
+		add_action( 'load-post.php', array( $this, 'load_post' ) );
 		
-		add_filter( 'the_posts', array( __CLASS__, 'the_posts' ) );
+		add_filter( 'the_posts', array( $this, 'the_posts' ) );
 
 	}
 	
@@ -61,11 +67,11 @@ class Simple_Sponsors_Link_To {
 	 * @param string $key post meta key
 	 * @return array an array of objects with post_id and meta_value properties
 	 */
-	public static function get_meta( $post_type_name, $meta_key ) {
+	function get_meta( $post_type_name, $meta_key ) {
 	
 		global $wpdb;
 
-		$array = $wpdb->get_results( $wpdb->prepare( "SELECT $wpdb->posts.ID, $wpdb->postmeta.meta_value FROM $wpdb->posts JOIN $wpdb->postmeta ON $wpdb->posts.ID = $wpdb->postmeta.post_id WHERE $wpdb->posts.post_type = %s AND $wpdb->postmeta.meta_key = %s", $post_type_name, $meta_key ) );
+		$array = $wpdb->get_results( $wpdb->prepare( "SELECT $wpdb->postmeta.post_id, $wpdb->postmeta.meta_value FROM $wpdb->postmeta JOIN $wpdb->posts ON $wpdb->posts.ID = $wpdb->postmeta.post_id WHERE $wpdb->posts.post_type = %s AND $wpdb->postmeta.meta_key = %s", $post_type_name, $meta_key ) );
 				
 		return $array;
 		
@@ -75,28 +81,28 @@ class Simple_Sponsors_Link_To {
 	 * Returns all sponsor url links for the current site
 	 * @return array an array of links, keyed by post ID
 	 */
-	public static function get_links() {
+	function get_links() {
 	
 		global $wpdb, $blog_id;
-		
+				
 		//if links have already been set for this site return them otherwise get from db
-		if ( !isset( self::$links[$blog_id] ) )
-			$links_to = self::get_meta( self::$post_type_name , '_sponsor-url' );			
+		if ( !isset( $this->links[$blog_id] ) )
+			$links_to = $this->get_meta( $this->post_type_name , '_sponsor-url' );			
 		else
-			return self::$links[$blog_id];
-		
+			return $this->links[$blog_id];
+					
 		//if no links return false
 		if ( !$links_to ) {
-			self::$links[$blog_id] = false;
+			$this->links[$blog_id] = false;
 			return false;
 		}
 		
 		//loop through links_to array adding to $links
 		foreach ( (array) $links_to as $link )
-			self::$links[$blog_id][$link->post_id] = $link->meta_value;
+			$this->links[$blog_id][$link->post_id] = $link->meta_value;
 		
 		//return links for the current site
-		return self::$links[$blog_id];
+		return $this->links[$blog_id];
 		
 	}
 	
@@ -104,28 +110,28 @@ class Simple_Sponsors_Link_To {
 	 * Returns all targets for the current site (i.e. target="_blank")
 	 * @return array an array of targets, keyed by post ID
 	 */
-	public static function get_targets() {
+	function get_targets() {
 	
 		global $wpdb, $links_to_target_cache, $blog_id;
-						
+												
 		//if targets have already been set for this site return them otherwise get from db
-		if ( !isset( self::$targets[$blog_id] ) )
-			$links_to_targets = self::get_meta( self::$post_type_name ,'_sponsor-links-to-target' );
+		if ( !isset( $this->targets[$blog_id] ) )
+			$links_to_targets = $this->get_meta( $this->post_type_name ,'_sponsor-links-to-target' );
 		else
-			return self::$targets[$blog_id];
-			
+			return $this->targets[$blog_id];
+									
 		//if no targets return false
 		if ( !$links_to_targets ) {
-			self::$targets[$blog_id] = false;
+			$this->targets[$blog_id] = false;
 			return false;
 		}
-
+				
 		//loop through links_to_targets array adding to $targets
-		foreach ( (array) $links_to_targets as $link )
-			self::$targets[$blog_id][$link->post_id] = $link->meta_value;
-
+		foreach ( $links_to_targets as $link )
+			$this->targets[$blog_id][$link->post_id] = $link->meta_value;
+		
 		//return targets for the current site
-		return self::$targets[$blog_id];
+		return $this->targets[$blog_id];
 		
 	}
 
@@ -134,8 +140,8 @@ class Simple_Sponsors_Link_To {
 	 *
 	 * @wp-action add_meta_boxes
 	 */
-	public static function add_meta_box() {
-		add_meta_box( 'sponsor-link', __( 'Sponsor Link', self::$text_domain  ), array( __CLASS__, 'do_meta_box' ), self::$post_type_name , 'normal', 'high' );
+	function add_meta_box() {
+		add_meta_box( 'sponsor-link', __( 'Sponsor Link', $this->text_domain  ), array( $this, 'do_meta_box' ), $this->post_type_name , 'normal', 'high' );
 	}
 
 	/**
@@ -144,7 +150,7 @@ class Simple_Sponsors_Link_To {
 	 * @param WP_Post $object Current post object
 	 * @param array $box Metabox information
 	 */
-	public static function do_meta_box( $object, $box ) {
+	function do_meta_box( $object, $box ) {
 	
 		$choice = get_post_meta( $object->ID, '_sponsor-links-to-choice', true );
 				
@@ -152,12 +158,12 @@ class Simple_Sponsors_Link_To {
 
 		wp_nonce_field( basename( __FILE__ ), 'sponsor-link' );
 ?>
-		<p><?php _e( 'Point this sponsor to:', self::$text_domain ); ?></p>
-		<p><label><input type="radio" id="sponsor-links-to-wp" name="sponsor-links-to-choice" value="wp" <?php checked( 'wp', $choice ); ?> /> <?php _e( 'Its normal WordPress URL', self::$text_domain ); ?></label></p>
-		<p><label><input type="radio" id="sponsor-links-to-sponsor-url" name="sponsor-links-to-choice" value="sponsor-url" <?php checked( 'sponsor-url', $choice  ); ?> /> <?php _e( 'The Sponsor URL', self::$text_domain ); ?></label></p>
+		<p><?php _e( 'Point this sponsor to:', $this->text_domain ); ?></p>
+		<p><label><input type="radio" id="sponsor-links-to-wp" name="sponsor-links-to-choice" value="wp" <?php checked( 'wp', $choice ); ?> /> <?php _e( 'Its normal WordPress URL', $this->text_domain ); ?></label></p>
+		<p><label><input type="radio" id="sponsor-links-to-sponsor-url" name="sponsor-links-to-choice" value="sponsor-url" <?php checked( 'sponsor-url', $choice  ); ?> /> <?php _e( 'The Sponsor URL', $this->text_domain ); ?></label></p>
 		<div style="margin-left: 30px;" id="sponsor-links-to-sponsor-url-section" class="">
-			<p><label><input type="radio" id="sponsor-links-to-self" name="sponsor-links-to-target" value="_self" <?php checked( '_self', $target ); ?> /> <?php _e( 'Open this link in the same window (default)', self::$text_domain ); ?></label></p>
-			<p><label><input type="radio" id="sponsor-links-to-new-window" name="sponsor-links-to-target" value="_blank" <?php checked( '_blank', $target ); ?> /> <?php _e( 'Open this link in a new window', self::$text_domain ); ?></label></p>
+			<p><label><input type="radio" id="sponsor-links-to-self" name="sponsor-links-to-target" value="_self" <?php checked( '_self', $target ); ?> /> <?php _e( 'Open this link in the same window (default)', $this->text_domain ); ?></label></p>
+			<p><label><input type="radio" id="sponsor-links-to-new-window" name="sponsor-links-to-target" value="_blank" <?php checked( '_blank', $target ); ?> /> <?php _e( 'Open this link in a new window', $this->text_domain ); ?></label></p>
 		</div>		
 <?php
 	}
@@ -168,7 +174,7 @@ class Simple_Sponsors_Link_To {
 	 * @wp-action save_post
 	 * @param int $post_id The ID of the current post being saved.
 	 */
-	public static function save_meta( $post_id ) {
+	function save_meta( $post_id ) {
 
 		/* Verify the nonce before proceeding. */
 		if ( !isset( $_POST['sponsor-link'] ) || !wp_verify_nonce( $_POST['sponsor-link'], basename( __FILE__ ) ) )
@@ -205,12 +211,12 @@ class Simple_Sponsors_Link_To {
 	 * @param int|object $post Either a post ID or a post object
 	 * @return string output URL
 	 */
-	public static function filter_link( $link, $post ) {
+	function filter_link( $link, $post ) {
 	
-		if ( $post->post_type == self::$post_type_name ) {
+		if ( $post->post_type == $this->post_type_name ) {
 	
-			$links = self::get_links();
-			
+			$links = $this->get_links();
+						
 			// Really strange, but page_link gives us an ID and post_link gives us a post object
 			$id = ( is_object( $post ) && $post->ID ) ? $post->ID : $post;
 	
@@ -226,10 +232,10 @@ class Simple_Sponsors_Link_To {
 	/**
 	 * Performs a redirect, if appropriate
 	 */
-	public static function template_redirect() {
+	function template_redirect() {
 	
-		//if ( !is_single() && !is_page() )
-		//	return;
+		if ( !is_single() && !is_page() )
+			return;
 
 		global $wp_query;
 
@@ -251,11 +257,11 @@ class Simple_Sponsors_Link_To {
 	 * @param string $pages the wp_list_pages() HTML block from WordPress
 	 * @return string the modified HTML block
 	 */
-	public static function wp_list_pages( $pages ) {
+	function wp_list_pages( $pages ) {
 	
 		$highlight = false;
-		$links = self::get_links();
-		$links_to_target_cache = self::get_targets();
+		$links = $this->get_links();
+		$links_to_target_cache = $this->get_targets();
 
 		if ( !$links && !$links_to_target_cache )
 			return $pages;
@@ -299,9 +305,9 @@ class Simple_Sponsors_Link_To {
 	/**
 	 * Filters nav menu
 	 */
-	public static function wp_nav_menu_objects( $items, $args ) {
+	function wp_nav_menu_objects( $items, $args ) {
 	
-		$links_to_target_cache = self::get_targets();
+		$links_to_target_cache = $this->get_targets();
 		$new_items = array();
 		
 		foreach ( $items as $item ) {
@@ -320,12 +326,12 @@ class Simple_Sponsors_Link_To {
 	/**
 	 * Display Notification
 	 */	
-	public static function load_post() {
+	function load_post() {
 	
 		if ( isset( $_GET['post'] ) ) {
 		
 			if ( get_post_meta( absint( $_GET['post'] ), '_sponsor-links-to-choice', true ) == 'sponsor-url' )
-				add_action( 'admin_notices', array( __CLASS__, 'notify_of_sponsor_link' ) );
+				add_action( 'admin_notices', array( $this, 'notify_of_sponsor_link' ) );
 			
 		}
 		
@@ -334,37 +340,37 @@ class Simple_Sponsors_Link_To {
 	/**
 	 * Notification
 	 */		
-	public static function notify_of_sponsor_link() {
-		?><div class="updated"><p><?php _e( '<strong>Note</strong>: This sponsor is pointing to the sponsor url.', self::$text_domain ); ?></p></div><?php
+	function notify_of_sponsor_link() {
+		?><div class="updated"><p><?php _e( '<strong>Note</strong>: This sponsor is pointing to the sponsor url.', $this->text_domain ); ?></p></div><?php
 	}
 
 	/**
 	 * Filter the_posts array of posts.
 	 */	
-	public static function the_posts( $posts ) {
-	
-		$links_to_target_cache = self::get_targets();
-										
+	function the_posts( $posts ) {
+		
+		$links_to_target_cache = $this->get_targets();
+																			
 		if ( is_array( $links_to_target_cache ) && count( $links_to_target_cache ) ) {
 		
 			$pids = array();
 			
 			foreach ( (array) $posts as $p )
 				$pids[$p->ID] = $p->ID;
-				
+								
 			$targets = array_keys( array_intersect_key( $links_to_target_cache , $pids ) );
-			
+									
 			if ( count( $targets ) ) {
 						
-				array_walk( $targets, array( __CLASS__, 'id_to_url_callback' ) );
+				array_walk( $targets, array( $this, 'id_to_url_callback' ) );
 				
 				$targets = array_unique( $targets );
 				
-				self::$targets_on_this_page = $targets;
+				$this->targets_on_this_page = $targets;
 				
 				wp_enqueue_script( 'jquery' );
 				
-				add_action( 'wp_head', array( __CLASS__, 'targets_in_new_window_via_js' ) );
+				add_action( 'wp_head', array( $this, 'targets_in_new_window_via_js' ) );
 				
 			}
 			
@@ -377,15 +383,15 @@ class Simple_Sponsors_Link_To {
 	/**
 	 * ??
 	 */	
-	public static function id_to_url_callback( &$val, $key ) {
+	function id_to_url_callback( &$val, $key ) {
 		$val = get_permalink( $val );
 	}
 
 	/**
 	 * Filters nav menu
 	 */	
-	public static function targets_in_new_window_via_js() {
-		?><script>(function($){var t=<?php echo json_encode( self::$targets_on_this_page ); ?>;$(document).ready(function(){var a=$('a');$.each(t,function(i,v){a.filter('[href="'+v+'"]').attr('target','_blank');});});})(jQuery);</script><?php
+	function targets_in_new_window_via_js() {
+		?><script>(function($){var t=<?php echo json_encode( $this->targets_on_this_page ); ?>;$(document).ready(function(){var a=$('a');$.each(t,function(i,v){a.filter('[href="'+v+'"]').attr('target','_blank');});});})(jQuery);</script><?php
 	}
 		
 
